33cd532443ff8cf66948ca3a250e63ce953531cd
Matheus Barão Fiorin <matheusfiiorin@gmail.com>
2019-11-22 00:57:52 -0300

    Basic auth methods

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,8 +1,9 @@
 const bcrypt = require('bcrypt');
 const express = require('express');
-const generateErrorResponse = require('./functions').generateErrorResponse;
 const model = require('../models/index');
 const router = express.Router();
+
+require('./functions')();
 
 router.post('/login', (req, res) => {
   const {
@@ -11,11 +12,11 @@ router.post('/login', (req, res) => {
   } = req.body;
 
   if (!email || !password) {
-    generateErrorResponse(401, req.url, "Empty fields", ["Email and/or password field is empty"], res);
+    return generateErrorResponse(401, req.url, "Empty fields", ["Email and/or password field is empty"], res);
   }
 
-  if (!email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g)) {
-    generateErrorResponse(401, req.url, "Invalid email", ["The email provided isn't a valid email"], res);
+  if (!isValidEmail(email)) {
+    return generateErrorResponse(401, req.url, "Invalid email", ["The email provided isn't a valid email"], res);
   }
 
   model.users.findOne({
@@ -25,10 +26,10 @@ router.post('/login', (req, res) => {
     })
     .then(user => {
       if (!user) {
-        generateErrorResponse(401, req.url, "Unauthorized", ["This username isn't registered."], res);
+        return generateErrorResponse(401, req.url, "Unauthorized", ["This username isn't registered."], res);
       }
       if (!bcrypt.compareSync(password, user.password)) {
-        generateErrorResponse(401, req.url, "Unauthorized", ["Wrong password."], res);
+        return generateErrorResponse(401, req.url, "Unauthorized", ["Wrong password."], res);
       }
       res.status(200).json({
         jwt: "jwt", // TODO
@@ -39,7 +40,7 @@ router.post('/login', (req, res) => {
       console.error({
         err
       });
-      generateErrorResponse(400, req.url, "Unknown error", [err], res);
+      return generateErrorResponse(400, req.url, "Unknown error", [err], res);
     })
 });
 
@@ -52,11 +53,15 @@ router.post('/register', (req, res) => {
   } = req.body;
 
   if (!fullname || !cpf || !email || !password) {
-    generateErrorResponse(401, req.url, "Empty fields", ["Every field is required."], res);
+    return generateErrorResponse(401, req.url, "Empty fields", ["Every field is required."], res);
   }
 
-  if (!email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g)) {
-    generateErrorResponse(401, req.url, "Invalid email", ["The email provided isn't a valid email"], res);
+  if (!isValidEmail(email)) {
+    return generateErrorResponse(401, req.url, "Invalid email", ["The email provided isn't a valid email"], res);
+  }
+
+  if (!isValidCPF(cpf)) {
+    return generateErrorResponse(401, req.url, "Invalid CPF", ["The CPF provided isn't a valid CPF"], res);
   }
 
   model.users.findOne({
@@ -66,7 +71,7 @@ router.post('/register', (req, res) => {
     })
     .then(user => {
       if (user) {
-        generateErrorResponse(401, req.url, "User already exists", ["CPF field is unique."], res);
+        return generateErrorResponse(401, req.url, "User already exists", ["CPF field is unique."], res);
       }
 
       let encriptedPassword = bcrypt.hashSync(password, 10);
@@ -89,8 +94,8 @@ router.post('/register', (req, res) => {
       console.error({
         err
       });
-      generateErrorResponse(400, req.url, "Unknown error", [err], res);
-    })
-})
+      return generateErrorResponse(400, req.url, "Unknown error", [err], res);
+    });
+});
 
 module.exports = router;
--- back-end/routes/functions.js
+++ back-end/routes/functions.js
@@ -1,10 +1,48 @@
-module.exports = {
-  generateErrorResponse: function (code, url, name, causes, res) {
+module.exports = function () {
+  this.isValidCPF = function (strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+    if (
+      strCPF == "00000000000" ||
+      strCPF == "11111111111" ||
+      strCPF == "22222222222" ||
+      strCPF == "33333333333" ||
+      strCPF == "44444444444" ||
+      strCPF == "55555555555" ||
+      strCPF == "66666666666" ||
+      strCPF == "77777777777" ||
+      strCPF == "88888888888" ||
+      strCPF == "99999999999" ||
+      strCPF.length != 11
+    )
+      return false;
+
+    for (var i = 1; i <= 9; i++)
+      Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if (Resto == 10 || Resto == 11) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++)
+      Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if (Resto == 10 || Resto == 11) Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+    return true;
+  };
+  this.isValidEmail = function (email) {
+    return email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g);
+  };
+  this.generateErrorResponse = function (code, url, name, causes, res) {
     return res.status(code).json({
       code,
       url,
       name,
       causes
     });
-  }
+  };
 }
